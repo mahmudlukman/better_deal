336f18a0b788c4f0395378d7cb43f9dda3317d62
Mahmud Lukman <mistamann49@gmail.com>
2023-11-29 14:04:40 +0100

    Login page UI done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage } from './Routes';
 
 const App = () => {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
