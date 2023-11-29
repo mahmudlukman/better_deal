65b4849ba6e6821b5b11ac4270aa3fe337ae3166
Mahmud Lukman <mistamann49@gmail.com>
2023-11-29 17:07:45 +0100

    express error handler setup complete

--- server/app.js
+++ server/app.js
@@ -1,7 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import ErrorHandler from './utils/ErrorHandler.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
 
 const app = express();
+
+// app.use(cors({
+//   origin: ['https://eshop-tutorial-pyri.vercel.app',],
+//   credentials: true
+// }));
+app.use(cors());
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+app.use(fileUpload({useTempFiles: true}))
+
 
 // config
 if (process.env.NODE_ENV !== 'PRODUCTION') {
@@ -10,4 +32,6 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
   });
 }
 
+// it's for ErrorHandling
+app.use(ErrorHandler);
 export default app;
