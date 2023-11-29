a3e487091efd86031ee6863eb504661fa89c6f8f
Mahmud Lukman <mistamann49@gmail.com>
2023-11-29 16:37:36 +0100

    backend server and mongodb connected

---
+++ server/db/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then((data) => {
+      console.log(`mongodb connected with server: ${data.connection.host}`);
+    });
+};
+
+export default connectDatabase;
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import app from "./app.js";
+import dotenv from 'dotenv'
+import connectDatabase from "./db/database.js";
+
+// Handling uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server for handling uncaught exception`);
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({
+    path: ".env",
+  });
+}
+
+connectDatabase()
+
+// create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT}`
+  );
+});
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandled promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
