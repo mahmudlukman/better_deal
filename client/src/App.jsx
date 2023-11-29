810739c6761d23db9eacfbe2f384306a034c64fd
Mahmud Lukman <mistamann49@gmail.com>
2023-11-29 14:43:08 +0100

    Register component complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LoginPage } from './Routes';
+import { LoginPage, SignupPage } from './Routes';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
