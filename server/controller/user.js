cf24277ad726a88036e24386454dd39d11a0f143
Mahmud Lukman <mistamann49@gmail.com>
2023-12-03 12:01:45 +0100

    all user controllers added

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,3 @@
-import express from 'express';
 import User from '../model/user.js';
 import jwt from 'jsonwebtoken';
 import catchAsyncErrors from '../middleware/catchAsyncErrors';
@@ -6,7 +5,6 @@ import cloudinary from 'cloudinary';
 import ErrorHandler from '../utils/ErrorHandler.js';
 import sendMail from '../utils/sendMail';
 import sendToken from '../utils/jwtToken';
-import { isAuthenticated, isAdmin } from '../middleware/auth';
 
 export const register = catchAsyncErrors(async (req, res, next) => {
   try {
@@ -85,6 +83,275 @@ export const activateUser = catchAsyncErrors(async (req, res, next) => {
     });
 
     sendToken(user, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// login user
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ErrorHandler('Please provide the all fields!', 400));
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      return next(new ErrorHandler("User doesn't exists!", 400));
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return next(
+        new ErrorHandler('Please provide the correct information', 400)
+      );
+    }
+
+    sendToken(user, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// load user
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return next(new ErrorHandler("User doesn't exists", 400));
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// log out user
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  try {
+    res.cookie('token', null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
+    res.status(201).json({
+      success: true,
+      message: 'Log out successful!',
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// update user info
+export const updateUserInfo = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { email, password, phoneNumber, name } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      return next(new ErrorHandler('User not found', 400));
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return next(
+        new ErrorHandler('Please provide the correct information', 400)
+      );
+    }
+
+    user.name = name;
+    user.email = email;
+    user.phoneNumber = phoneNumber;
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// update user avatar
+export const updateAvatar = catchAsyncErrors(async (req, res, next) => {
+  try {
+    let existsUser = await User.findById(req.user.id);
+    if (req.body.avatar !== '') {
+      const imageId = existsUser.avatar.public_id;
+
+      await cloudinary.v2.uploader.destroy(imageId);
+
+      const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: 'avatars',
+        width: 150,
+      });
+
+      existsUser.avatar = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+    }
+
+    await existsUser.save();
+
+    res.status(200).json({
+      success: true,
+      user: existsUser,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// update user addresses
+export const updateUserAddresses = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    const sameTypeAddress = user.addresses.find(
+      (address) => address.addressType === req.body.addressType
+    );
+    if (sameTypeAddress) {
+      return next(
+        new ErrorHandler(`${req.body.addressType} address already exists`)
+      );
+    }
+
+    const existsAddress = user.addresses.find(
+      (address) => address._id === req.body._id
+    );
+
+    if (existsAddress) {
+      Object.assign(existsAddress, req.body);
+    } else {
+      // add the new address to the array
+      user.addresses.push(req.body);
+    }
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// delete user address
+export const deleteUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const addressId = req.params.id;
+
+    await User.updateOne(
+      {
+        _id: userId,
+      },
+      { $pull: { addresses: { _id: addressId } } }
+    );
+
+    const user = await User.findById(userId);
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// update user password
+export const updateUserPassword = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select('+password');
+
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler('Old password is incorrect!', 400));
+    }
+
+    if (req.body.newPassword !== req.body.confirmPassword) {
+      return next(
+        new ErrorHandler("Password doesn't matched with each other!", 400)
+      );
+    }
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: 'Password updated successfully!',
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// find user information with the userId
+export const findUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// all users --- for admin
+export const findAllUsers = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const users = await User.find().sort({
+      createdAt: -1,
+    });
+    res.status(201).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// delete users --- admin
+export const deleteUsers = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return next(new ErrorHandler('User is not available with this id', 400));
+    }
+
+    const imageId = user.avatar.public_id;
+
+    await cloudinary.v2.uploader.destroy(imageId);
+
+    await User.findByIdAndDelete(req.params.id);
+
+    res.status(201).json({
+      success: true,
+      message: 'User deleted successfully!',
+    });
   } catch (error) {
     return next(new ErrorHandler(error.message, 500));
   }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,55 @@
-import express from "express";
-import {activateUser, register} from '../controller/user.js'
+import express from 'express';
+import {
+  activateUser,
+  deleteUser,
+  deleteUsers,
+  findAllUsers,
+  findUser,
+  getUser,
+  loginUser,
+  logout,
+  register,
+  updateAvatar,
+  updateUserAddresses,
+  updateUserInfo,
+  updateUserPassword,
+} from '../controller/user.js';
+import { isAdmin, isAuthenticated } from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
-userRouter.post("/register", register);
-userRouter.post("/activation", activateUser);
+userRouter.post('/register', register);
+userRouter.post('/activation', activateUser);
+userRouter.post('/login-user', loginUser);
+userRouter.get('/user-info/:id', findUser);
+userRouter.get(
+  '/admin-all-users',
+  isAuthenticated,
+  isAdmin('Admin'),
+  findAllUsers
+);
+userRouter.delete(
+  '/delete-user/:id',
+  isAuthenticated,
+  isAdmin('Admin'),
+  deleteUsers
+);
+userRouter.get(
+  '/admin-all-users',
+  isAuthenticated,
+  isAdmin('Admin'),
+  findAllUsers
+);
+userRouter.get('/logout', logout);
+userRouter.get('/getUser', isAuthenticated, getUser);
+userRouter.patch('/update-user-info', isAuthenticated, updateUserInfo);
+userRouter.patch('/update-avatar', isAuthenticated, updateAvatar);
+userRouter.patch(
+  '/update-user-addresses',
+  isAuthenticated,
+  updateUserAddresses
+);
+userRouter.delete('/delete-user-address/:id', isAuthenticated, deleteUser);
+userRouter.patch('/update-user-password', isAuthenticated, updateUserPassword);
 
 export default userRouter;
