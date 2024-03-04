b0702496f0873afaa07dd18d647ae296d848caa2
Mahmud Lukman <mistamann49@gmail.com>
2024-03-04 16:52:01 +0100

    Delete Product from shop added

--- server/controllers/product.ts
+++ server/controllers/product.ts
@@ -64,3 +64,31 @@ export const getAllProductsInShop = catchAsyncError(
     }
   }
 );
+
+// delete product of a shop
+export const deleteProductInShop = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const product = await ProductModel.findById(req.params.id);
+
+      if (!product) {
+        return next(new ErrorHandler('Product is not found with this id', 404));
+      }
+
+      for (let i = 0; 1 < product.images.length; i++) {
+        const result = await cloudinary.v2.uploader.destroy(
+          product.images[i].public_id
+        );
+      }
+
+      await product.deleteOne();
+
+      res.status(201).json({
+        success: true,
+        message: 'Product Deleted successfully!',
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- server/routes/product.route.ts
+++ server/routes/product.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createProduct, getAllProductsInShop } from '../controllers/product';
+import { createProduct, deleteProductInShop, getAllProductsInShop } from '../controllers/product';
 
 const productRouter = express.Router();
 
 productRouter.post('/create-product', createProduct);
 productRouter.get('/get-all-products-shop/:id', getAllProductsInShop);
+productRouter.delete('/delete-shop-product/:id', deleteProductInShop);
 
 export default productRouter;
