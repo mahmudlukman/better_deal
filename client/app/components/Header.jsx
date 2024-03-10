947575c0175899a5d9061c48cc8949dd214d505d
Mahmud Lukman <mistamann49@gmail.com>
2024-03-10 10:10:21 +0100

    signup ui added

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -7,28 +7,32 @@ import {
   AiOutlineSearch,
   AiOutlineShoppingCart,
 } from 'react-icons/ai';
+import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
+import { BiMenuAltLeft } from 'react-icons/bi';
+import { CgProfile } from 'react-icons/cg';
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchData, setSearchData] = useState(null);
+  const [active, setActive] = useState(false);
 
   const handleSearchChange = (e) => {
     const term = e.target.value;
     setSearchTerm(term);
 
     const filteredProducts = productData.filter((product) =>
-        product.name.toLowerCase().includes(term.toLowerCase())
-      );
+      product.name.toLowerCase().includes(term.toLowerCase())
+    );
     setSearchData(filteredProducts);
   };
 
-  // window.addEventListener('scroll', () => {
-  //   if (window.scrollY > 70) {
-  //     setActive(true);
-  //   } else {
-  //     setActive(false);
-  //   }
-  // });
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 70) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  });
 
   return (
     <div className={`${styles.section}`}>
@@ -77,6 +81,105 @@ const Header = () => {
                 })}
             </div>
           ) : null}
+        </div>
+        <div className={`${styles.button}`}>
+          <Link href={`seller`}>
+            <h1 className="text-[#fff] flex items-center">
+              Become Seller
+              <IoIosArrowForward className="ml-1" />
+            </h1>
+          </Link>
+        </div>
+      </div>
+      <div
+        className={`${
+          active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
+        } transition hidden 800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}
+      >
+        <div
+          className={`${styles.section} relative ${styles.noramlFlex} justify-between`}
+        >
+          {/* categories */}
+          <div onClick={() => {}}>
+            <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+              <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+              <button
+                className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
+              >
+                All Categories
+              </button>
+              <IoIosArrowDown
+                size={20}
+                className="absolute right-2 top-4 cursor-pointer"
+                // onClick={() => setDropDown(!dropDown)}
+              />
+              {/* {dropDown ? (
+                <DropDown
+                  categoriesData={categoriesData}
+                  setDropDown={setDropDown}
+                />
+              ) : null} */}
+            </div>
+          </div>
+          {/* navitems */}
+          <div className={`${styles.noramlFlex}`}>
+            {/* <Navbar active={activeHeading} /> */}
+          </div>
+
+          <div className="flex">
+            <div className={`${styles.noramlFlex}`}>
+              <div
+                className="relative cursor-pointer mr-[15px]"
+                onClick={() => {}}
+              >
+                <AiOutlineHeart size={30} color="rgb(255 255 255 / 83%)" />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  {/* {wishlist && wishlist.length} */}
+                </span>
+              </div>
+            </div>
+
+            <div className={`${styles.noramlFlex}`}>
+              <div
+                className="relative cursor-pointer mr-[15px]"
+                onClick={() => {}}
+              >
+                <AiOutlineShoppingCart
+                  size={30}
+                  color="rgb(255 255 255 / 83%)"
+                />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  {/* {cart && cart.length} */}
+                </span>
+              </div>
+            </div>
+
+            <div className={`${styles.noramlFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                {/* {isAuthenticated ? (
+                  <Link to="/profile">
+                    <img
+                      src={`${user?.avatar?.url}`}
+                      className="w-[35px] h-[35px] rounded-full"
+                      alt=""
+                    />
+                  </Link>
+                ) : (
+                  <Link to="/login">
+                    <CgProfile size={30} color="rgb(255 255 255 / 83%)" />
+                  </Link>
+                )} */}
+              </div>
+            </div>
+
+            {/* cart popup */}
+            {/* {openCart ? <Cart setOpenCart={setOpenCart} /> : null} */}
+
+            {/* wishlist popup */}
+            {/* {openWishlist ? (
+              <Wishlist setOpenWishlist={setOpenWishlist} />
+            ) : null} */}
+          </div>
         </div>
       </div>
     </div>
