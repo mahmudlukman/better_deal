2b77bc325de43e0ed2baf7178875175551a2be73
Mahmud Lukman <mistamann49@gmail.com>
2024-04-04 14:19:46 +0100

    cart sidebar added

--- client/app/components/Cart/Cart.jsx
+++ client/app/components/Cart/Cart.jsx
@@ -1,32 +1,29 @@
-import React, { useState } from "react";
-import { RxCross1 } from "react-icons/rx";
-import { IoBagHandleOutline } from "react-icons/io5";
-import { HiOutlineMinus, HiPlus } from "react-icons/hi";
-import styles from "../../styles/styles";
+import React, { useState } from 'react';
+import { RxCross1 } from 'react-icons/rx';
+import { IoBagHandleOutline } from 'react-icons/io5';
+import { HiOutlineMinus, HiPlus } from 'react-icons/hi';
+import styles from '../../styles/styles';
 import Link from 'next/link';
-// import { useDispatch, useSelector } from "react-redux";
-// import { addTocart, removeFromCart } from "../../redux/actions/cart";
-import { toast } from "react-hot-toast";
+import { toast } from 'react-hot-toast';
 
 const Cart = ({ setOpenCart }) => {
-
   const cartData = [
     {
-      name: "IPhone 14 pro max 256gb and 8gb ram silver",
-      description: "test",
-      price: "9999",
+      name: 'IPhone 14 pro max 256gb and 8gb ram silver',
+      description: 'test',
+      price: '9999',
     },
     {
-      name: "IPhone 14 pro max 256gb and 8gb ram silver",
-      description: "test",
-      price: "3429",
+      name: 'IPhone 14 pro max 256gb and 8gb ram silver',
+      description: 'test',
+      price: '3429',
     },
     {
-      name: "IPhone 14 pro max 256gb and 8gb ram silver",
-      description: "test",
-      price: "4329",
+      name: 'IPhone 14 pro max 256gb and 8gb ram silver',
+      description: 'test',
+      price: '4329',
     },
-  ]
+  ];
   // const { cart } = useSelector((state) => state.cart);
   // const dispatch = useDispatch();
 
@@ -77,31 +74,29 @@ const Cart = ({ setOpenCart }) => {
 
               {/* cart Single Items */}
               <br />
-              {/* <div className="w-full border-t">
+              <div className="w-full border-t">
                 {cartData &&
                   cartData.map((i, index) => (
                     <CartSingle
                       key={index}
                       data={i}
-                      quantityChangeHandler={quantityChangeHandler}
-                      removeFromCartHandler={removeFromCartHandler}
                     />
                   ))}
-              </div> */}
+              </div>
             </div>
 
-            {/* <div className="px-5 mb-3">
-              checkout buttons
+            <div className="px-5 mb-3">
+              {/* checkout buttons */}
               <Link href="/checkout">
                 <div
                   className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}
                 >
                   <h1 className="text-[#fff] text-[18px] font-[600]">
-                    Checkout Now (USD${totalPrice})
+                    Checkout Now (USD$1080)
                   </h1>
                 </div>
               </Link>
-            </div> */}
+            </div>
           </>
         )}
       </div>
@@ -109,86 +104,25 @@ const Cart = ({ setOpenCart }) => {
   );
 };
 
-const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
-  const [value, setValue] = useState(data.qty);
-  const totalPrice = data.discountPrice * value;
-
-  const increment = (data) => {
-    if (data.stock < value) {
-      toast.error("Product stock limited!");
-    } else {
-      setValue(value + 1);
-      const updateCartData = { ...data, qty: value + 1 };
-      quantityChangeHandler(updateCartData);
-    }
-  };
-
-  const decrement = (data) => {
-    setValue(value === 1 ? 1 : value - 1);
-    const updateCartData = { ...data, qty: value === 1 ? 1 : value - 1 };
-    quantityChangeHandler(updateCartData);
-  };
-
-  return (
-    <div className="border-b p-4">
-      <div className="w-full flex items-center">
-        <div>
-          <div
-            className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.noramlFlex} justify-center cursor-pointer`}
-            onClick={() => increment(data)}
-          >
-            <HiPlus size={18} color="#fff" />
-          </div>
-          <span className="pl-[10px]">{data.qty}</span>
-          <div
-            className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
-            onClick={() => decrement(data)}
-          >
-            <HiOutlineMinus size={16} color="#7d879c" />
-          </div>
-        </div>
-        <img
-          src={`${data?.images[0]?.url}`}
-          alt=""
-          className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
-        />
-        <div className="pl-[5px]">
-          <h1>{data.name}</h1>
-          <h4 className="font-[400] text-[15px] text-[#00000082]">
-            ${data.discountPrice} * {value}
-          </h4>
-          <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
-            US${totalPrice}
-          </h4>
-        </div>
-        <RxCross1
-          className="cursor-pointer"
-          onClick={() => removeFromCartHandler(data)}
-        />
-      </div>
-    </div>
-  );
-};
-
 const CartSingle = ({ data }) => {
-  const [value, setValue] = useState(data.qty);
-  const totalPrice = data.discountPrice * value;
+  const [value, setValue] = useState(1);
+  const totalPrice = data.price * value;
 
-  const increment = (data) => {
-    if (data.stock < value) {
-      toast.error("Product stock limited!");
-    } else {
-      setValue(value + 1);
-      const updateCartData = { ...data, qty: value + 1 };
-      quantityChangeHandler(updateCartData);
-    }
-  };
+  // const increment = (data) => {
+  //   if (data.stock < value) {
+  //     toast.error('Product stock limited!');
+  //   } else {
+  //     setValue(value + 1);
+  //     const updateCartData = { ...data, qty: value + 1 };
+  //     quantityChangeHandler(updateCartData);
+  //   }
+  // };
 
-  const decrement = (data) => {
-    setValue(value === 1 ? 1 : value - 1);
-    const updateCartData = { ...data, qty: value === 1 ? 1 : value - 1 };
-    quantityChangeHandler(updateCartData);
-  };
+  // const decrement = (data) => {
+  //   setValue(value === 1 ? 1 : value - 1);
+  //   const updateCartData = { ...data, qty: value === 1 ? 1 : value - 1 };
+  //   quantityChangeHandler(updateCartData);
+  // };
 
   return (
     <div className="border-b p-4">
@@ -196,27 +130,27 @@ const CartSingle = ({ data }) => {
         <div>
           <div
             className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.noramlFlex} justify-center cursor-pointer`}
-            onClick={() => increment(data)}
+            onClick={() => setValue(value + 1)}
           >
             <HiPlus size={18} color="#fff" />
           </div>
-          <span className="pl-[10px]">{data.qty}</span>
+          <span className="pl-[10px]">{value}</span>
           <div
             className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
-            onClick={() => decrement(data)}
+            onClick={() => setValue(value === 1 ? 1 : value - 1)}
           >
             <HiOutlineMinus size={16} color="#7d879c" />
           </div>
         </div>
         <img
-          src={`${data?.images[0]?.url}`}
+          src="https://bonik-react.vercel.app/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png"
           alt=""
           className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
         />
         <div className="pl-[5px]">
           <h1>{data.name}</h1>
           <h4 className="font-[400] text-[15px] text-[#00000082]">
-            ${data.discountPrice} * {value}
+            ${data.price} * {value}
           </h4>
           <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
             US${totalPrice}
